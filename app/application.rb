269320d439f0d46d89206d19708920e99c64db8d
Chris Cardle <cdcardle@gmail.com>
2018-02-22 16:28:05 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -7,7 +7,6 @@ class Application
 
     if req.path.match(/items/)
       item_name = req.path.split("/items/")
-      binding.pry
       if @@items.include?(item_name)
         return item_name.price
       else
