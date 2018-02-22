1c0e1741cc97aa931e11c9850f3c5d7a32c8f163
Chris Cardle <cdcardle@gmail.com>
2018-02-22 16:26:56 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -6,7 +6,7 @@ class Application
     req = Rack::Request.new(env)
 
     if req.path.match(/items/)
-      item_name = req.path.split("/items/").last
+      item_name = req.path.split("/items/")
       if @@items.include?(item_name)
         return item_name.price
       else
